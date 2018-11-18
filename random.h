6815fd0fe33af7ee54c9d6930d3b91469970fa3e
Derrick Pallas <derrick@pallas.us>
2018-11-17 21:58:48 -0800

    new: add random
    This utility class automatically seeds the Xrand48 RNG from /dev/urandom and
    provides access to both longs and doubles.

---
+++ random.h
@@ -0,0 +1,32 @@
+#ifndef LACE__RANDOM_H
+#define LACE__RANDOM_H
+
+#include "try.h"
+#include "scoped.h"
+
+#include <cstdio>
+#include <cstring>
+
+namespace lace {
+
+class random {
+public:
+    random(const char * file = "/dev/urandom") {
+        scoped<FILE, int, ::fclose> fd(TRY_PTR(fopen, file, "rb"));
+        TRY(fread, state, sizeof state, 1, fd);
+    }
+
+    random(unsigned short seed[3]) { memcpy(state, seed, sizeof state); }
+
+    long l() { return jrand48(state); }
+    double d() { return erand48(state); }
+
+    long max() const { return RAND_MAX; }
+
+private:
+    unsigned short state[3];
+};
+
+} // namespace lace
+
+#endif//LACE__RANDOM_H
