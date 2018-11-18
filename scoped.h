810e2da8294f50f642492d4a08b4d6e34bf24501
Derrick Pallas <derrick@pallas.us>
2018-11-17 21:12:19 -0800

    new: add scoped
    This utility class uses RAII to destroy C PODs.  During construction, it
    takes a pointer, which it now owns.  On destruction, if that pointer is
    still set, it calls a destructor defined by the template.

---
+++ scoped.h
@@ -0,0 +1,32 @@
+#ifndef LACE__SCOPED_H
+#define LACE__SCOPED_H
+
+#include <algorithm>
+
+namespace lace {
+
+template <typename T, typename R, R (*D)(T*)>
+class scoped {
+public:
+    scoped(T* _) : p(_) { }
+    ~scoped() { if (p) D(p); }
+
+    operator T* const () const { return p; }
+
+    T* reset(T* r = NULL) {
+        std::swap(r, p);
+        return r;
+    }
+
+    void clear(T* r = NULL) {
+        if (p) D(p);
+        p = r;
+    }
+
+private:
+    T* p;
+};
+
+} // namespace lace
+
+#endif//LACE__SCOPED_H
