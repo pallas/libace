f344b898d5716229e9b3a49c1c0c63c06f883a12
Derrick Pallas <derrick@pallas.us>
2018-11-17 21:59:04 -0800

    test: update divide to use random
    This change updates test_divide to use a singleton random rather than
    srand/rand seeded by getpid.

--- test_divide.cc
+++ test_divide.cc
@@ -1,18 +1,20 @@
 #include "divider.h"
+#include "random.h"
+#include "singleton.h"
 #include <cassert>
-#include <cstdlib>
-#include <unistd.h>
+#include <cstdint>
+
+lace::random & rng = lace::singleton<lace::random>().instance();
 
 uint64_t r(unsigned i) {
   uint64_t m = uint64_t(1) << i%64;
   if (i<64)
       return m;
-  uint64_t x = rand() + (uint64_t(rand())<<32);
+  uint64_t x = rng.l() + (uint64_t(rng.l())<<32);
   return (m-1) & x | m;
 }
 
 int main(int, char*[]) {
-    srand(getpid());
     for (int i = 0 ; i < 1<<10 ; ++i) {
       volatile uint64_t x = r(i);
       lace::divider d(x);
