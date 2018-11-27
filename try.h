efafc8083fdbc54cfa450a72788af29ca3c56756
Derrick Pallas <derrick@pallas.us>
2018-11-26 21:29:25 -0800

    try: add TRY_ABORT for use with noexcept

--- try.h
+++ try.h
@@ -13,6 +13,12 @@
   _r; \
 })
 
+#define TRY_ABORT(f, ...) ({ \
+  typeof (f(__VA_ARGS__)) _r = f(__VA_ARGS__); \
+  if (_r < 0) perror(#f), abort(); \
+  _r; \
+})
+
 #define TRY_ERR(e, f, ...) ({ \
   typeof (f(__VA_ARGS__)) _r = f(__VA_ARGS__); \
   if (_r < 0 && errno != e) perror(#f), throw std::runtime_error(strerror(errno)); \
