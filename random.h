c9d52c8ce2f95024048da1bd16c2153953d844cd
Derrick Pallas <derrick@pallas.us>
2020-08-13 20:11:39 -0700

    random: replace jrand48/erand48 with MSWS RNG
    Rather than using the 48-bit libc PRNGs, swap in the Middle Square Weyl
    Sequence RNG [1], which have 64-bit seeds and 128-bits of state.  Like the
    old generator, this will still be initalized with system entropy by default.
    
    Note: when generating 64-bit values, it is not clear to me if concatenating
    sequential 32-bit outputs from the generator is kosher.  If not, it will
    obviously have to change.
    
    [1] https://arxiv.org/pdf/1704.00358v5.pdf

--- random.h
+++ random.h
@@ -9,6 +9,7 @@
 #include <cstring>
 
 #include <unistd.h>
+#include <stdint.h>
 
 #ifdef __linux__
 #include <sys/syscall.h>
@@ -22,26 +23,50 @@ public:
     random(const char * file = NULL) {
 #ifdef SYS_getrandom
         if (!file)
-            if (!TRY_ERR(ENOSYS, syscall, SYS_getrandom, state, sizeof state, 0))
+            if (!TRY_ERR(ENOSYS, syscall, SYS_getrandom, &state, sizeof state, 0))
                 return;
 #endif
 
         scoped<FILE, int, ::fclose> fd(TRY_PTR(fopen, pick(file, "/dev/urandom"), "rb"));
-        TRY(fread, state, sizeof state, 1, fd);
+        TRY(fread, &state, sizeof state, 1, fd);
     }
 
-    random(unsigned short seed[3]) { memcpy(state, seed, sizeof state); }
+    typedef uint64_t seed_t;
+    random(const seed_t seed) {
+        memcpy(&state.seed, &seed, sizeof state.seed);
+    }
 
     bool b() { return l() >= 0; }
-    long l() { return jrand48(state); }
-    double d() { return erand48(state); }
 
-    unsigned long ul() { return (unsigned long)l(); }
+    int32_t i32() { return (int32_t)u32(); }
+    uint32_t u32() { return (uint32_t)r(); }
 
-    long max() const { return RAND_MAX; }
+    int64_t i64() { return (int64_t)u64(); }
+    uint64_t u64() { return (r()<<32) | (r()>>32); }
+
+    long l() { return (long)ul(); }
+    unsigned long ul() {
+        return sizeof(unsigned long) <= sizeof(uint32_t) ? u32()
+             : sizeof(unsigned long) <= sizeof(uint64_t) ? u64()
+             : (__builtin_unreachable(), 0);
+    }
+
+    double d() { return double(u64()>>11)/double(UINT64_MAX>>11); }
 
 private:
-    unsigned short state[3];
+    struct state_t {
+        seed_t seed;
+        uint64_t weyl;
+    } state;
+
+    template <uint64_t STEP = 0xb5ad4eceda1ce2a9UL>
+    uint64_t r() {
+        state.seed *= state.seed;
+        state.weyl += STEP;
+        state.seed += state.weyl;
+        state.seed = (state.seed << 32) | (state.seed >> 32);
+        return state.seed;
+    }
 };
 
 } // namespace lace
