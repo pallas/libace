fccbb31a65f679b1302e9b52b1a232ad9d925c41
Derrick Pallas <derrick@pallas.us>
2020-08-15 17:24:23 -0700

    test: add naïve statistics to randomness test
    We use Welford's online algorithm to calculate mean & variance for a perfect
    coin vs. randomly generating bools, unsigned longs, floats, and doubles.
    We also generate a student's t test value that bits are generated in equal
    proportions.  These tests are very basic and may not be implemented
    correctly, but do seem to show that the generator is working as expected.

--- test_random.cc
+++ test_random.cc
@@ -3,16 +3,121 @@
 #include <cassert>
 #include <cstdint>
 #include <cstdlib>
+#include <climits>
+#include <cmath>
+#include <iostream>
 
 lace::random & rng = lace::singleton<lace::random>::instance();
 
-int main(int, char*[]) {
-  for (int i = 0 ; i < 1<<10 ; ++i) {
-#ifndef NDEBUG
-    lace::random o = rng;
-#endif
-    volatile uint64_t x = rng.l();
-    assert(x == o.l());
+template <typename T = double>
+class welford {
+public:
+  welford() : _count(0), _mean(T(0.0)), _squared_distance(T(0.0)) { }
+
+  welford & operator<< (T value) {
+    T delta = value - _mean;
+    _mean += delta / ++_count;
+    _squared_distance += delta * (value - _mean);
+    return *this;
   }
+
+  T samples() const { return _count; }
+  T mean() const { return _mean; }
+  T variance() const { return _squared_distance / T(_count - 1); }
+  T deviation() const { return std::sqrt(variance()); }
+  T error() const { return std::sqrt(variance()/samples()); }
+  T gosset(T u) { return (_mean - u) * std::sqrt(samples()/variance()); }
+
+private:
+  unsigned _count;
+  T _mean, _squared_distance;
+};
+
+int main(int, char*[]) {
+  {{
+    welford<> w;
+    const int n = 1<<20;
+    for (int i = 0 ; i < n ; ++i)
+      w << i % 2;
+
+    std::cout << "perfect coin"
+      << ",\t mean=" << w.mean()
+      << ",\t variance=" << w.variance()
+      << ",\t error=" << w.error()
+      << ",\t t-test=" << w.gosset(0.5)
+      << std::endl;
+  }}
+
+  {{
+    welford<> w;
+    const int n = 1<<20;
+    for (int i = 0 ; i < n ; ++i)
+      w << rng.b();
+
+    std::cout << "pop"
+      << ",\t mean=" << w.mean()
+      << ",\t variance=" << w.variance()
+      << ",\t error=" << w.error()
+      << ",\t t-test=" << w.gosset(0.5)
+      << std::endl;
+  }}
+
+  {{
+    const unsigned bits = sizeof(long)*CHAR_BIT;
+    welford<> w[bits];
+    const int n = 1<<20;
+    for (int i = 0 ; i < n ; ++i) {
+      unsigned long x = rng.ul();
+      for (int j = 0 ; j < bits ; ++j)
+        w[j] << ((x & (0x1ULL<<j))>>j);
+    }
+
+    welford<> wx;
+    for (int j = 0 ; j < bits ; ++j) {
+      wx << w[j].mean();
+      std::cout << "bit " << j
+        << ",\t mean=" << w[j].mean()
+        << ",\t variance=" << w[j].variance()
+        << ",\t error=" << w[j].error()
+        << ",\t t-test=" << w[j].gosset(0.5)
+        << std::endl;
+    }
+
+    std::cout << "bits"
+      << ",\t mean=" << wx.mean()
+      << ",\t variance=" << wx.variance()
+      << ",\t error=" << wx.error()
+      << ",\t t-test=" << wx.gosset(0.5)
+      << std::endl;
+  }}
+
+  {{
+    welford<> w;
+    const int n = 1<<20;
+    for (int i = 0 ; i < n ; ++i)
+      w << rng.f();
+
+    std::cout << "f"
+      << ",\t mean=" << w.mean()
+      << ",\t variance=" << w.variance()
+      << ",\t error=" << w.error()
+      << ",\t t-test=" << w.gosset(0.5)
+      << std::endl;
+  }}
+
+  {{
+    welford<> w;
+    const int n = 1<<20;
+    for (int i = 0 ; i < n ; ++i)
+      w << rng.d();
+
+    std::cout << "d"
+      << ",\t mean=" << w.mean()
+      << ",\t variance=" << w.variance()
+      << ",\t error=" << w.error()
+      << ",\t t-test=" << w.gosset(0.5)
+      << std::endl;
+  }}
+
   return EXIT_SUCCESS;
 }
